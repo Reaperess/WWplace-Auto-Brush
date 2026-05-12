f1f26f235c634249970913995ffb686bffef27f1
The Reaperess <106025809+Reaperess@users.noreply.github.com>
2026-05-12 17:04:42 +0300

    Create wwplace.brush.meta.js

---
+++ wwplace.brush.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         WW Place AutoBrush V2
+// @namespace    https://github.com/Reaperess/WWplace-Auto-Brush
+// @version      1.0.0
+// @downloadURL  https://raw.githubusercontent.com/Reaperess/WWplace-Auto-Brush/main/wwplace.brush.user.js
+// @updateURL    https://raw.githubusercontent.com/Reaperess/WWplace-Auto-Brush/main/wwplace.brush.meta.js
+// ==/UserScript==
